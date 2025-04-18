f5bdd94742a1bbc2c10c62ae1188697bc92a2035
danj73-digital-lab <danielabonomo@gmail.com>
2025-04-18 09:45:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.getElementById("filter-stars").addEventListener("change", function() {
-    const selectedStars = this.value;
-    document.querySelectorAll(".recensione").forEach(recensione => {
-        const stars = recensione.querySelector("p:nth-of-type(2)").textContent.length;
-        recensione.style.display = (selectedStars === "all" || stars == selectedStars) ? "block" : "none";
-    });
-});
+...
